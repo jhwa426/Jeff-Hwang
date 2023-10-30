329fde63567c24bb88bcf0bc7a92dc117581cc1a
Jeff Hwang <wodud6359@gmail.com>
2023-10-30 14:26:01 +1300

    personal-website

---
+++ personal-website/src/components/ProjectDetails/ProjectDetails.js
@@ -0,0 +1,67 @@
+import { Container, Wrapper, Title, Date, Description, Image, Label, Skills, Skill, Members, Member, MemberImage, MemberName, MemberDetails, ButtonGroup, Button } from './ProjectDetailsStyledComponent';
+import { CloseRounded, GitHub, LinkedIn } from '@mui/icons-material';
+import { Modal } from '@mui/material';
+
+
+const ProjectDetails = ({ openModal, setOpenModal }) => {
+    const project = openModal?.project;
+
+    return (
+        <Modal open={true} onClose={() => setOpenModal({ state: false, project: null })}>
+            <Container onClick={() => setOpenModal({ state: false, project: null })}>
+                <Wrapper >
+                    <CloseRounded
+                        style={{
+                            position: "absolute",
+                            top: "10px",
+                            right: "20px",
+                            cursor: "pointer",
+                            "font-size": "25px"
+                        }}
+                        onClick={() => setOpenModal({ state: false, project: null })}
+                    />
+                    <Image src={project?.img} />
+                    <Title>{project?.title}</Title>
+                    <Date>{project?.date}</Date>
+                    <Description>{project?.description}</Description>
+                    <Skills>
+                        {project?.skills.map((skill) => (
+                            <Skill>{skill}</Skill>
+                        ))}
+                    </Skills>
+
+                    {project.descriptionDetails?.map((detail, index) => (
+                        <Description>{detail}</Description>
+                    ))}
+
+                    {project.members && (
+                        <>
+                            <Label>Members</Label>
+                            <Members>
+                                {project?.members.map((member) => (
+                                    <Member>
+                                        <MemberImage src={member.img} />
+                                        <MemberName>{member.name}</MemberName>
+                                        <MemberDetails href={member.github} target="_blank" rel="noopener noreferrer">
+                                            <GitHub />
+                                        </MemberDetails>
+                                        <MemberDetails href={member.linkedin} target="_blank" rel="noopener noreferrer">
+                                            <LinkedIn />
+                                        </MemberDetails>
+                                    </Member>
+                                ))}
+                            </Members>
+                        </>
+                    )}
+
+                    <ButtonGroup>
+                        <Button href={project?.github} target="_blank" rel="noopener noreferrer">View the code</Button>
+                        <Button href={project?.website} target="_blank" rel="noopener noreferrer">View the project</Button>
+                    </ButtonGroup>
+                </Wrapper>
+            </Container>
+        </Modal>
+    )
+}
+
+export default ProjectDetails;
