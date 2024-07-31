80a2f892a44b2a257d4089021ddd0e0c225dde48
Jeff Hwang <wodud6359@gmail.com>
2024-07-31 14:52:56 +1200

    data

--- personal-website/src/AppStyledComponent.js
+++ personal-website/src/AppStyledComponent.js
@@ -6,6 +6,7 @@ export const Body = styled.div`
     height: 100%;
     overflow-x: hidden;
 `;
+
 export const Wrapper = styled.div`
     background: linear-gradient(38.73deg, rgba(204, 0, 187, 0.15) 0%, rgba(201, 32, 184, 0) 50%), linear-gradient(141.27deg, rgba(0, 70, 209, 0) 50%, rgba(0, 70, 209, 0.15) 100%);
     width: 100%;
